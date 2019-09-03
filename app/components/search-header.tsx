847e1f4f000b56a6b17c7cb06f8b65cdd8c6cee4
Shrouk Khan <shroukkhan@gmail.com>
2019-09-04 01:05:19 +0700

    add tab bar buttons

--- app/components/search-header.tsx
+++ app/components/search-header.tsx
@@ -1,42 +1,95 @@
 import React from "react"
-import { TextInput, View } from "react-native"
+import { TouchableOpacity, View } from "react-native"
 import EStyleSheet from "react-native-extended-stylesheet"
+import { Text, TextInput } from "react-native-paper"
+import { color } from "../theme"
 
+
+const TabBarButton = ({ text, selected, type, navigation }) => {
+  return (
+    <TouchableOpacity activeOpacity={1}
+                      onPress={() => navigation.setParams({ searchMode: type })}
+                      style={[styles.button, { backgroundColor: selected ? color.primary : color.background }]}>
+      <Text style={{ color: selected ? color.background : undefined }}>
+        {text}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 const SearchHeader = ({ navigation, collapsible }) => {
   // @ts-ignore
   const { searchText } = navigation.state.params ? navigation.state.params : {}
+  // @ts-ignore
+  const { searchMode } = navigation.state.params ? navigation.state.params : {}
   return (
     <View
       style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "white",
-          flex: 1,
-          borderRadius: 10,
-          margin: 10,
-          justifyContent: "center",
-        }}>
+      <View style={styles.internalContainer}>
         <TextInput
-          style={{ paddingHorizontal: 20 }}
-          placeholder="Search Movies"
+          style={styles.textInputStyle}
+          label="Search Movies"
           value={searchText}
           onChangeText={text => navigation.setParams({ searchText: text })}
         />
+        <View style={styles.buttonContainer}>
+          <TabBarButton navigation={navigation} type={"top"} key={"top"} selected={searchMode === "top"}
+                        text={"Top Movies"}/>
+          <TabBarButton navigation={navigation} type={"popular"} key={"popular"} selected={searchMode === "popular"}
+                        text={"Popular Movies"}/>
+          <TabBarButton navigation={navigation} type={"search"} key={"search"} selected={searchMode === "search"}
+                        text={"Search"}/>
+        </View>
       </View>
     </View>
   )
 }
 
-const styles =  EStyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
     width: "100%",
-    height: "100%",
-    paddingHorizontal: 75,
+    height: 120,
     justifyContent: "center",
-    backgroundColor: "green"
+    backgroundColor: color.background,
+    borderBottomWidth: 0.75,
+    borderBottomColor: color.charcoal,
   },
+  internalContainer: {
+    // backgroundColor:color.background,
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowColor: "black",
+    // shadowOpacity: 0.5,
+    // height:100
 
+  },
+  buttonContainer: {
+    backgroundColor: color.background,
+    width: "100%",
+    height: 50,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    backgroundColor: color.background,
+    width: "30%",
+    borderColor: color.charcoal,
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    // padding: 15,
+    height: 35,
+    marginRight: 5,
+  },
+  textInputStyle: {
+    backgroundColor: color.background,
+    margin: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    marginHorizontal: 15,
+  },
 
 })
 
--- app/theme/color.ts
+++ app/theme/color.ts
@@ -1,7 +1,6 @@
 import { palette } from "./palette"
 
 export const color = {
-
   background: "#fff",
   clear: "rgba(0,0,0,0)",
   facebook: "#3b5998",
@@ -25,66 +24,6 @@ export const color = {
   drawer: "rgba(30, 30, 29, 0.95)",
   borderBottomColor: "#ecf0f1",
   deepBlueSkyColor: "#1E90FF",
-
-  // /OKKAMI Colors
-  black: "#000000",
-  blackOpacity20Percent: "rgba(0,0,0,.2)",
-  blackOpacity40Percent: "rgba(0,0,0,.4)",
-  blackOpacity50Percent: "rgba(0,0,0,.5)",
-  blackOpacity60Percent: "rgba(0,0,0,.6)",
-  blackOpacity75Percent: "rgba(0,0,0,.75)",
-  blackOpacity90Percent: "rgba(0,0,0,.9)",
-  darkGrayColor: "#333333", // rgb(51, 51, 51) - sakul king - back button - navigation Title - rightButton use this
-  okkamiBaseColor: "#c7242a", // red by sakul king rgb(199, 36, 42)
-  baseRedColor: "#c7242a", // red by michael santoso
-  facebookBlue: "#3b5998",
-  lineGreen: "#00B900", // line by michael - i think this one is correct - use this is better because match with the line icon
-  white: "#ffffff",
-  white50Percent: "rgba(255, 255, 255, 0.50)", // no use
-  borderGray: "#D7D7D7", // michael - for picture holder border color rgb(215, 215, 215)
-  backgroundGray: "#d3d3d3", // for gradation - sakul king
-  okkamiBlack2: "#1c1c1c", // rgb(28, 28, 28) - sakul king - for some text
-  orTextColor: "#363636", // rgb(54, 54, 54) - sakul king
-  textSmall: "#636363", // rgb(99, 99, 99) - Terms text or dont have account or by signing up - sakul king
-  lineOrDivider: "#e1e1e1", // for divide OR  rgb(225, 225, 225) - sakul king
-  grayBackgroundShadow: "#aaaaaa", // rgb(170, 170, 170) - sakul king - line separator
-  helpColor: "#484848", // rgb(72, 72, 72) - sakul king
-  outline: "#424242", // rgb(66, 66, 66) - sakul king
-  titleTextColor: "#707070", // rgb(112, 112, 112) - sakul king - we do not - upload picture text
-  titleTextColorSignUp: "rgb(175,175,175)", // rgb(112, 112, 112) - sakul king - we do not - upload picture text
-  phoneSignUpTextColor: "#686868", // rgb(104, 104, 104) - sakul king
-  countryCodeHolder: "#d6d6d6", // rgb(214, 214, 214) - sakul king
-  okkamiBlack: "#0d0d0d", // rgb(13, 13, 13) - sakul king
-  checkIconColor: "#66a719", // rgb(102, 167, 25) - sakul king
-  lineLocationDetector: "#cecece", // rgb(206, 206, 206) - sakul king
-  orTextColorLocationDetector: "#555555", // rgb(85, 85, 85) - sakul king
-  lineLocationDetector2: "#c5c5c5", // rgb(197, 197, 197) - sakul king
-  roundLocationRectangle: "#372f2f", // rgb(55, 47, 47) - sakul king
-  navBarTabBarTextColor: "rgba(255, 255, 255, 0.502)", // - sakul king
-  seeAllColor: "#a1a1a1", // rgb(161, 161, 161) - sakul king
-  ratingColor: "#f7941d", // rgb(247, 148, 29) - sakul king
-  favSaveCopy: "rgba(28, 28, 28, 0)",
-  logoMaskColor: "#f6f7f9", // rgb(246, 247, 249) - sakul king
-  profileBottomTab: "#010202", // rgb(1, 2, 2) - sakul king
-  tabGrayColor: "#c2c2c2", // rgb(194, 194, 194) - sakul king
-  panelSub: "#ebebeb",
-  tvDarkGray: "#363636",
-  tvGray: "#535353",
-  tvTextButtonGray: "#E5E5E5",
-  tvButtonBorder: "#7A7A7A",
-  hotelLobbyBgTransparent: "rgba(52,57,64, 0.9)",
-  overlay: "rgba(0,0,0,0.7)",
-
-  modalPickerTitleColor: "rgb(52,52,52)",
-  modalPickerOptionColor: "rgb(55,55,55)",
-
-  hubConnecting: "rgb(198,201,199)",
-  hubOffline: "rgb(237,93,98)",
-  hubConnected: "rgb(16,123,53)",
-  hubActivity: "rgb(67,140,185)",
-
-  underLineTextField: "rgba(255, 255, 255, 0.50)",
-  lightGrayPicker: "#f5f5f5",
   /**
    * The palette is available to use, but prefer using the name.
    */
